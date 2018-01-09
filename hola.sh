f420e74269e47d020b40bca042d7343b746f1ef1
Jenkins <dcmikki@gmail.com>
2018-01-09 19:01:47 +0000

    actualizar TODO el sistema con ejemcutando ansible desde la linea de commandos para ubuntu/debia system

--- hola.sh
+++ hola.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Hola Mundo"
+sleep 3
 ansible localhost -m setup
+sleep 5
 ansible localhost -s -m shell -a "apt-get update && apt-get upgrade -y && apt-get autoclean && apt-get autoremove -y"
 
