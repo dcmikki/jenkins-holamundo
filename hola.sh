12903d8a96eef7be85796e90745cb8aa9d1b8864
dcmikki <dcmikki@gmail.com>
2018-01-13 19:34:14 +0000

    quitano uptualizar sistema

--- hola.sh
+++ hola.sh
@@ -2,6 +2,3 @@
 echo "Hola Mundo"
 sleep 3
 ansible localhost -m setup
-sleep 5
-ansible localhost -s -m shell -a "apt-get update && apt-get upgrade -y && apt-get autoclean && apt-get autoremove -y"
-
