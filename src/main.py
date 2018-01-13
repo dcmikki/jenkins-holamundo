bbaf543ee9660ae52c171b50fc89f45a9e0714c2
dcmikki <dcmikki@gmail.com>
2018-01-13 20:04:10 +0000

    instalado aplicacion flask en un entorno virtual y funciona. (http:/localhost:5000)

--- src/main.py
+++ src/main.py
@@ -3,4 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hola Mundo!"
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
+
+
+
