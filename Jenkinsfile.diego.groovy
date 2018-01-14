11a684c276eed9bdddd783637ee5ce5d01933ac9
dcmikki <dcmikki@gmail.com>
2018-01-14 21:58:28 +0000

    notas que hacer con el Jenkinsfile

--- Jenkinsfile.diego.groovy
+++ Jenkinsfile.diego.groovy
@@ -1,3 +1,6 @@
+// Este es el primer primer borrador del Jenkinsfile. Se tiene que mirar todo la sintaxis
+// Ver como poner coretament los comandos bash, multi-linea, variables, path, etc
+// Lo mejor es trocear el pipeline e ir testeando cada 'stage' en cadena
 pipeline {
     agent master
     stages {
