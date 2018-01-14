5d5a16a3bb497a3e4045763f47e4dfa8a2803131
dcmikki <dcmikki@gmail.com>
2018-01-14 12:48:52 +0000

    agregamos los test en python and requirements

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def suma(a,b):
+  return a+b
+
 @app.route("/")
 def hello():
-    return "Hola Mundo!"
+    res = suma(3,2)
+    return "Hola Mundo! %s" %(res)
 
 
 
